66dae814db5b3a098541706af7fc75a1f7e33def
Iván L. Murillo <diskman199@gmail.com>
2022-01-30 10:08:06 -0600

    Fixed and added projects

--- js/projects.js
+++ js/projects.js
@@ -1,5 +1,5 @@
 (async()=>{
-    let projects = await fetch('../sources/projects.json');
+    let projects = await fetch('sources/projects.json');
     projects = await projects.json();
 
     let content = '';
@@ -10,7 +10,31 @@
 
         project.description.forEach(d => {
             properties += `<li> ${d} </li>`
-        })
+        });
+
+        let links = '';
+        if(project.url){
+            links += `
+            <a href="${ project.url }" target="_blank" title="Link to the webpage"
+                class="repository mx-2">
+                <i class="fas fa-globe"></i> Visit the webpage
+            </a>`;
+        }
+        if(project.video){
+            links += `
+            <a href="${ project.video }" target="_blank" title="Link to YouTube"
+                class="repository mx-2">
+                <i class="fab fa-youtube"></i> View demo
+            </a>`;
+        }
+        if(project.repository){
+            links += `
+            <a href="${ project.repository }" target="_blank" title="Link to Github"
+                class="repository mx-2">
+                <i class="fab fa-github"></i> Visit the repository
+            </a>`;
+        }
+        
         content += `
         <div class="card m-3">
             <div class="card-horizontal">
@@ -18,17 +42,17 @@
                     <img class="card-icon" src="${ project.image }">
                 </div>
                 <div class="card-body">
-                    <h4 class="card-title font-weight-bold border-animation-black">POSBE</h4>
+                    <h4 class="card-title font-weight-bold border-animation-black">
+                        ${ project.name }
+                        <span class="year">${ project.year }</span>
+                    </h4>
                     <ul class="card-text">
                         ${ properties }
                     </ul>
                 </div>
             </div>
             <div class="card-footer">
-                <a href="${ project.repository }" target="_blank" title="Link to Github"
-                    class="repository">
-                    <i class="fab fa-github"></i> Visit the repository
-                </a>
+                ${links}
             </div>
         </div>
         `
